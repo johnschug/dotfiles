e6978d78b1397ee2e53bb73a1595d4353e633ebb
John Schug <john.ips.schug@gmail.com>
2019-10-25 03:53:55 -0700

    Fix tmux truecolor over ssh

--- tmux/.tmux-conf.sh
+++ tmux/.tmux-conf.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if command -v tput >/dev/null 2>&1; then
-  COLORS=$(tput "-T${1:-$TERM}" colors)
+  colors=$(tput "-T${1:-$TERM}" colors)
 else
-  COLORS=
+  colors=
 fi
 
 if infocmp tmux >/dev/null 2>&1; then
@@ -10,65 +10,66 @@ if infocmp tmux >/dev/null 2>&1; then
 else
   tmux set-option -g default-terminal screen
 fi
-if [ "$COLORS" -ge 16 ]; then
+if [ "$colors" -ge 16 ]; then
   if infocmp tmux-16color >/dev/null 2>&1; then
     tmux set-option -g default-terminal tmux-16color
   else
     tmux set-option -g default-terminal screen-16color
   fi
 fi
-if [ "$COLORS" -ge 256 ]; then
+if [ "$colors" -ge 256 ]; then
   if infocmp tmux-256color >/dev/null 2>&1; then
     tmux set-option -g default-terminal tmux-256color
   else
     tmux set-option -g default-terminal screen-256color
   fi
 fi
-if [ "$COLORTERM" = "truecolor" ] || [ "$COLORS" -gt 256 ]; then
+if [ "$COLORTERM" = "truecolor" ] || [ "$colors" -gt 256 ]; then
   if infocmp tmux-direct >/dev/null 2>&1; then
     tmux set-option -g default-terminal tmux-direct
   fi
+  tmux set-options -sa ",${1:-$TERM}:RGB"
 fi
 
-if [ "$COLORTERM" = "truecolor" ] || [ "$COLORS" -gt 256 ] || { [ "$COLORS" -ge 256 ] && [ -n "$NOPALETTE" ]; }; then
-  COLOR0="#232629"
-  COLOR1="#eee8d5"
-  COLOR2="#31363b"
-  COLOR3="#93a1a1"
-  COLOR4="#586e75"
-  COLOR5="#657b83"
-  COLOR6="#1d99f3"
-  COLOR7="#2c3e50"
-  SEARCH="#2980b9"
-elif [ "$COLORS" -ge 16 ]; then
-  COLOR0="brightblack"
-  COLOR1="white"
-  COLOR2="black"
-  COLOR3="brightcyan"
-  COLOR4="brightgreen"
-  COLOR5="brightyellow"
-  COLOR6="blue"
-  COLOR7="brightmagenta"
-  SEARCH="blue"
+if [ "$COLORTERM" = "truecolor" ] || [ "$colors" -gt 256 ] || { [ "$colors" -ge 256 ] && [ -n "$NOPALETTE" ]; }; then
+  color0="#232629"
+  color1="#eee8d5"
+  color2="#31363b"
+  color3="#93a1a1"
+  color4="#586e75"
+  color5="#657b83"
+  color6="#1d99f3"
+  color7="#2c3e50"
+  search="#2980b9"
+elif [ "$colors" -ge 16 ]; then
+  color0="brightblack"
+  color1="white"
+  color2="black"
+  color3="brightcyan"
+  color4="brightgreen"
+  color5="brightyellow"
+  color6="blue"
+  color7="brightmagenta"
+  search="blue"
 else
-  COLOR0="black"
-  COLOR1="white"
-  COLOR2="black"
-  COLOR3="white"
-  COLOR4="white"
-  COLOR5="white"
-  COLOR6="cyan"
-  COLOR7="blue"
-  SEARCH="blue"
+  color0="black"
+  color1="white"
+  color2="black"
+  color3="white"
+  color4="white"
+  color5="white"
+  color6="cyan"
+  color7="blue"
+  search="blue"
 fi
-tmux set-option -g message-style "bg=$COLOR2,fg=$COLOR3"
-tmux set-option -g mode-style "fg=$SEARCH,reverse"
-tmux set-option -g message-command-style "bg=$COLOR2,fg=$COLOR3"
-tmux set-option -g status-left-style "bg=$COLOR0,reverse"
-tmux set-option -g status-right-style "bg=$COLOR0,reverse"
-tmux set-option -g status-style "bg=$COLOR2,fg=$COLOR3"
-tmux set-option -wg window-style "bg=$COLOR0,fg=$COLOR1"
-tmux set-option -wg window-status-current-style "bg=$COLOR7,bold"
+tmux set-option -g message-style "bg=$color2,fg=$color3"
+tmux set-option -g mode-style "fg=$search,reverse"
+tmux set-option -g message-command-style "bg=$color2,fg=$color3"
+tmux set-option -g status-left-style "bg=$color0,reverse"
+tmux set-option -g status-right-style "bg=$color0,reverse"
+tmux set-option -g status-style "bg=$color2,fg=$color3"
+tmux set-option -wg window-style "bg=$color0,fg=$color1"
+tmux set-option -wg window-status-current-style "bg=$color7,bold"
 
-tmux set-option -g status-left "#[fg=$COLOR4] #S #[fg=$COLOR5] #{window_width}x#{window_height} "
-tmux set-option -g status-right "#{?client_prefix,#[fg=$COLOR6] P ,}${SSH_CONNECTION:+ #h }#[fg=$COLOR5] #(cut -f1-4 -d' ' /proc/loadavg) #[fg=$COLOR4] %F %R "
+tmux set-option -g status-left "#[fg=$color4] #S #[fg=$color5] #{window_width}x#{window_height} "
+tmux set-option -g status-right "#{?client_prefix,#[fg=$color6] P ,}${SSH_CONNECTION:+ #h }#[fg=$color5] #(cut -f1-4 -d' ' /proc/loadavg) #[fg=$color4] %F %R "
