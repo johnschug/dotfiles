bf5b1fc0ac50b38698f36e7a4204d518c5d7223e
John Schug <me@jschug.dev>
2026-06-07 04:42:28 -0700

    Update .config/fish/functions/fish_user_key_bindings.fish

--- private_dot_config/private_fish/private_functions/private_fish_user_key_bindings.fish
+++ private_dot_config/private_fish/private_functions/private_fish_user_key_bindings.fish
@@ -1,10 +1,10 @@
 function fish_user_key_bindings
-    bind -M insert \ca beginning-of-line
-    bind -M insert \ce end-of-line
-    bind -M insert \cp history-prefix-search-backward
-    bind -M insert \cn history-prefix-search-forward
-    bind -M insert \ep history-token-search-backward
-    bind -M insert \en history-token-search-forward
-    bind -M insert \ct 'cd ../; commandline -f repaint'
-    bind -M insert \co 'prevd; commandline -f repaint'
+    bind -M insert ctrl-a beginning-of-line
+    bind -M insert ctrl-e end-of-line
+    bind -M insert alt-k history-token-search-backward
+    bind -M insert alt-j history-token-search-forward
+    bind -M insert alt-K history-prefix-search-backward
+    bind -M insert alt-J history-prefix-search-forward
+    bind -M insert ctrl-t 'cd ../; commandline -f repaint'
+    bind -M insert ctrl-o 'prevd; commandline -f repaint'
 end
